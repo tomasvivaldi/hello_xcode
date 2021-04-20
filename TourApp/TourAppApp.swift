fe2a3dfb2ef92762e47aa124f101aae483e9124e
Tomas Vivaldi <tomasvivaldi@Tomass-MacBook-Air.local>
2021-04-20 23:22:42 +0900

    Initialized and working with Pinak

---
+++ TourApp/TourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourAppApp.swift
+//  TourApp
+//
+//  Created by Tomas Vivaldi on 2021/04/20.
+//
+
+import SwiftUI
+
+@main
+struct TourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
